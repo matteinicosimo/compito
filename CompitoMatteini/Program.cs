2a9b136f90909f3bca26d8e098ec7456649ca005
matteinicosimo <cosimomatteini28@gmail.com>
2018-12-10 09:40:02 +0100

    modifica ciclo e commenti

--- CompitoMatteini/Program.cs
+++ CompitoMatteini/Program.cs
@@ -6,10 +6,10 @@ namespace CompitoMatteini
     {
         static void Main(string[] args)
         {
-            //Ciclo per visualizzare numeri da 0 a 9
-            for (int i = 0; i < 10; i++)
+            //Ciclo visualizzazione dei numeri --> 0 a 9
+            for (int j = 0; j < 10; j++)
             {
-                Console.WriteLine("Var contatore I >> " + i);
+                Console.WriteLine("Var contatore I >> " + j);
             }
         }
     }
