49a51d216517d2488afbfb9697f6e8d35d2fcde1
matteinicosimo <cosimomatteini28@gmail.com>
2018-12-10 09:35:47 +0100

    aggiunta commento

--- CompitoMatteini/Program.cs
+++ CompitoMatteini/Program.cs
@@ -6,6 +6,7 @@ namespace CompitoMatteini
     {
         static void Main(string[] args)
         {
+            //Ciclo per visualizzare numeri da 0 a 9
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Var contatore I >> " + i);
